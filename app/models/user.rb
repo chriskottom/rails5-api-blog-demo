9c6dd1c28891e7c799b89309ab6a8c9ab0095329
chriskottom <chris@chriskottom.com>
2016-12-03 22:00:55 +0100

    add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :full_name, presence: true
+
+  has_many :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203101435) do
+ActiveRecord::Schema.define(version: 20161203205644) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "category"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
